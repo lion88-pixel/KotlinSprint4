2a940b568314bb19c321734e4186e1f4a948ed6d
lion88 <lion_alex1988@mail.ru>
2025-07-23 06:31:51 +0300

    KS-19-2 first option

---
+++ src/main/kotlin/lesson_19/lesson_19_2.kt
@@ -0,0 +1,31 @@
+package org.example.lesson_19
+
+enum class ProductCategory {
+    CLOTHING {
+        override fun getUserFriendlyName(): String = "Одежда"
+    },
+    STATIONERY {
+        override fun getUserFriendlyName(): String = "Канцелярские товары"
+    },
+    MISCELLANEOUS {
+        override fun getUserFriendlyName(): String = "Разное"
+    };
+
+    abstract fun getUserFriendlyName(): String
+}
+
+class Product(val name: String, val id: Int, val category: ProductCategory) {
+    fun displayProductInfo() {
+        println("Название: $name, ID: $id, Категория: ${category.getUserFriendlyName()}")
+    }
+}
+
+fun main() {
+    val product1 = Product("Футболка", 123, ProductCategory.CLOTHING)
+    val product2 = Product("Ручка", 456, ProductCategory.STATIONERY)
+    val product3 = Product("Брелок", 789, ProductCategory.MISCELLANEOUS)
+
+    product1.displayProductInfo()
+    product2.displayProductInfo()
+    product3.displayProductInfo()
+}
