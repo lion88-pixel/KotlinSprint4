2c41b549f2b499326edf901f19029ea120de6aff
lion88 <lion_alex1988@mail.ru>
2025-07-27 23:35:45 +0300

    KS-19-1 third option

--- src/main/kotlin/lesson_19/lesson_19_1.kt
+++ src/main/kotlin/lesson_19/lesson_19_1.kt
@@ -9,7 +9,7 @@ enum class FishType {
 
 fun main() {
     println("Добавь в аквариум следующие виды рыб:")
-    FishType.values().forEach { fish ->
+    FishType.entries.forEach { fish ->
         println("- ${fish.name}")
     }
 }
